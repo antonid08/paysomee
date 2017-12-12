65ea506aeec9ca3498712e453cf5b44b644e8129
Antonenko Ilya <iantonenko@qulix.com>
2017-12-12 08:53:50 +0300

    Terminal. Transaction sum validation added.

--- terminal/src/main/java/com/paysomee/terminal/ui/MainActivity.java
+++ terminal/src/main/java/com/paysomee/terminal/ui/MainActivity.java
@@ -1,7 +1,11 @@
 package com.paysomee.terminal.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.paysomee.terminal.R;
 import com.paysomee.terminal.ui.payment.PaymentActivity;
+import com.paysomee.terminal.utils.Utils;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +14,7 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Activity with field to input amount of payment and button to process payment.
@@ -18,17 +23,19 @@ import android.widget.TextView;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private EditText sum;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        final EditText sum = findViewById(R.id.sum);
+        sum = findViewById(R.id.sum);
 
         findViewById(R.id.process).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PaymentActivity.start(MainActivity.this, Float.valueOf(sum.getText().toString()));
+                showPaymentActivity();
             }
         });
 
@@ -36,11 +43,32 @@ public class MainActivity extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if ((event != null
                     && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
-                    PaymentActivity.start(MainActivity.this, Float.valueOf(sum.getText().toString()));
+
+                    showPaymentActivity();
                 }
                 return false;
             }
         });
+    }
+
+    private void showPaymentActivity() {
+        List<String> errors = validateFields();
+        if (errors.isEmpty()) {
+            PaymentActivity.start(MainActivity.this, Float.valueOf(sum.getText().toString()));
+        } else {
+            Toast.makeText(MainActivity.this, Utils.listToString(errors, "\n"), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private List<String> validateFields() {
+        List<String> errors = new ArrayList<>();
+
+        if (sum.getText().toString().isEmpty()) {
+            errors.add(getString(R.string.empty_sum_error));
+        }
+
+        return errors;
     }
 
 }
